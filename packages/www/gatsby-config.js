341eb792a6158271e7abefe19fbaab9ef68981f9
Muhibullah Khan Kamali <57056374+Muhibullah-09@users.noreply.github.com>
2020-11-24 21:25:26 +0500

    update gatsby-config.js

--- packages/www/gatsby-config.js
+++ packages/www/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports={
     plugins:[
         {
-      resolve: `gatsby-plugin-create-client-paths`,
-      options: { prefixes: [`/app/*`] },
+      resolve: [`gatsby-plugin-create-client-paths`,`gatsby-plugin-apollo`],
+      options: { prefixes: [`/app/*`] , uri: "https://muhibullahkhan-todoapp.netlify.app/.netlify/functions/graphql"},
     },
     `gatsby-plugin-fontawesome-css`
     ]
